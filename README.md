56fde92f38a9c1f8e3cef8bfaa5ecea2e9875789
AleksanderJanusz <oleksanderjanusz@gmail.com>
2023-03-24 13:28:17 +0100

    Guessing game 2 v1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Guess the number game
+1) Start game using main.py
+2) Pick number from 0 to 1000
+3) I am guessing you have to prowide answers:
+	1 or too small - if my number is too small
+	2 or too big - if my number is too big
+	3 or you win - if I win
+	capitalization does not matter
+4) Have fun!
---
+++ main.py
@@ -0,0 +1,57 @@
+"""Gues the number game. You pick number from 0 to 1000 and I will guess it"""
+from random import randint
+
+
+def _get_answer() -> int:
+    """
+    Function get answer and validate the value.
+    :return: one of the cases
+    """
+    good_answers = ["too small", "too big", "you win"]
+    while True:
+        answer_ = input("Enter your answer: ").lower()
+        try:
+            answer_ = int(answer_)
+        except ValueError:
+            for id_, values_ in enumerate(good_answers):
+                if answer_ == values_:
+                    return id_
+            print("Incorrect answer!")
+        else:
+            if 0 < answer_ < 4:
+                return answer_ - 1
+            print("Incorrect answer!")
+
+
+def main() -> None:
+    """
+    Explain rules and start guessing. Check case from answer funktion and keep guessing.
+    :return: None
+    """
+    print("Think about integer number from 0 to 1000, and I will have guessed in maximum 10 tries")
+    print("You can answer \n1 - too small \n2 - too big \n3 - you win\n\n")
+    min_ = 0
+    max_ = 1000
+    guess_num = randint(min_, max_)
+    while True:
+
+        print(f"Your number is: {guess_num}")
+        case_ = _get_answer()
+
+        if not case_:
+            min_ = guess_num + 1
+        elif case_ == 1:
+            max_ = guess_num - 1
+        else:
+            print("Wygrałem!")
+            return None
+        try:
+            guess_num = randint(min_, max_)
+        except ValueError:
+            print("Do not cheat! \nTry again.")
+            min_ = 0
+            max_ = 1000
+            guess_num = randint(min_, max_)
+
+
+main()
